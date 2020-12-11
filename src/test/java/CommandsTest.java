3d87d968369f84caebb0354405c56d13ea00e01c
MikheilSoziashvili <mixeilsoziashvili@gmail.com>
2020-12-11 14:48:15 +0400

    Completed tests

---
+++ src/test/java/CommandsTest.java
@@ -0,0 +1,57 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class CommandsTest {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("http://the-internet.herokuapp.com/context_menu");
+        driver.manage().window().maximize();
+        WebElement clickEl = driver.findElement(By.id("hot-spot"));
+        Actions action = new Actions(driver);
+        action.contextClick(clickEl).perform();
+        driver.switchTo().alert().accept();
+        action.click().perform();
+    }
+
+    @Test
+    public void secondTest() throws InterruptedException {
+        driver.navigate().to("http://the-internet.herokuapp.com/dynamic_controls");
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/form[2]/button")).click();
+        Thread.sleep(5000);
+        System.out.println(driver.findElement(By.id("message")).getText());
+    }
+
+    @Test
+    public void thirdTest() {
+        driver.navigate().to("http://uitestpractice.com/Students/Actions");
+        WebElement ertiani = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[3]/ol/li[1]"));
+        WebElement tormeti = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[3]/ol/li[12]"));
+        Actions action = new Actions(driver);
+        action.moveToElement(ertiani).clickAndHold().moveToElement(tormeti).release().build().perform();
+        if (ertiani.getCssValue("background-color") != "bisque") {
+            System.out.println("Background Changed");
+        } else System.out.println("Not Changed");
+    }
+
+    @AfterClass
+    public void afterTest() {
+        driver.manage().deleteAllCookies();
+        driver.quit();
+    }
+}
