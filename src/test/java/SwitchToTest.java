4330a8d4bd37934a882cd01dac4b8675b798edf6
MikheilSoziashvili <mixeilsoziashvili@gmail.com>
2020-12-18 11:10:40 +0400

    switchtotest commit

---
+++ src/test/java/SwitchToTest.java
@@ -0,0 +1,43 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SwitchToTest {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("http://the-internet.herokuapp.com/iframe");
+        driver.switchTo().frame(driver.findElement(By.id("mce_0_ifr")));
+        final WebElement pTagInFrame = driver.findElement(By.cssSelector("p"));
+        pTagInFrame.clear();
+        pTagInFrame.sendKeys("Here Goes");
+        driver.switchTo().defaultContent();
+        driver.findElement(By.xpath("//button[@title='Align center']")).click();
+    }
+
+    @Test
+    public void secondTest() {
+        driver.navigate().to("https://demoqa.com/alerts");
+        driver.findElement(By.xpath("//button[@id='alertButton']")).click();
+        driver.switchTo().alert().accept();
+    }
+
+    @AfterClass
+    public void afterTests() {
+        driver.manage().deleteAllCookies();
+        driver.quit();
+    }
+}
